252e78e228b614575a224824d150bf8b50d8a6df
Conner685 <cponton@my.bcit.ca>
2025-05-04 14:09:48 -0700

    Fixed dotenv import and typos in seperate imports

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-require('dotenv');
-const express = requre('express');
+require('dotenv').config;
+const express = require('express');
 const session = require('express-session');
 const db = require('connect-mongo');
 const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const secretSession = process.env.SESSION_CODE;
+
+if (!secretSession) {
+  throw new Error('SESSION_CODE is not defined in environment variables');
+}
+
+const port = process.env.port || 3000;
+
+const app = express();
+
+
+
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
