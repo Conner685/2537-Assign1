46296c79d27fd4a904cf7a4a1b48b4a6a67b55de
Conner685 <cponton@my.bcit.ca>
2025-05-04 16:32:01 -0700

    Connected Mongo DB with environmental variables

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
 const secretSession = process.env.SESSION_CODE;
+const dbSecret = process.env.MONGO_SECRET;
+const dbUser = process.env.MONGO_USER;
+const dbPassword = process.env.MONGO_PASSWORD;
+const dbHost = process.env.MONGO_HOST;
 
 if (!secretSession) {
   throw new Error('SESSION_CODE is not defined in environment variables');
@@ -15,9 +19,28 @@ const port = process.env.port || 3000;
 
 const app = express();
 
+const expireTime = 1000 * 60 * 60;
 
+const mongoDB = db.create({
+  mongoUrl: `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/sessions`,
+	crypto: {
+		secret: dbSecret
+	}
+});
+
+app.use(session({ 
+  secret: secretSession,
+store: mongoDB, 
+saveUninitialized: false, 
+resave: true
+}
+));
 
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req,res) => {
+  res.send("<h1>World Hello!</h1>");
+});
 
 
 
