45d963598d9bfb7c73587e3291b447c3be5916e9
Conner685 <cponton@my.bcit.ca>
2025-05-04 17:09:34 -0700

    Repeated bug fixiong and trails to make it run

--- databaseConnections.js
+++ databaseConnections.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 
-const dbHost = process.env.MONGODB_HOST;
-const dbUser = process.env.MONGODB_USER;
-const dbPassword = process.env.MONGODB_PASSWORD;
+const { MongoClient } = require('mongodb');
 
-const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/?retryWrites=true`;
-var database = new MongoClient(atlasURI, {});
-module.exports = {database};
+async function connectToDatabase() {
+  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri);
+  
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db(process.env.MONGODB_DATABASE);
+  } catch (error) {
+    console.error('Connection error:', error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectToDatabase;
--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');const bcrypt = require('bcrypt');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
 const saltRounds = 12;
 const Joi = require("joi");
+const connectToDatabase = require('./databaseConnections');
 
 const secretSession = process.env.SESSION_CODE;
 const dbSecret = process.env.MONGO_SECRET;
 const dbUser = process.env.MONGO_USER;
 const dbPassword = process.env.MONGO_PASSWORD;
 const dbHost = process.env.MONGO_HOST;
-const data = process.env.MONGODB_DATABASE;
+const dbName = process.env.MONGODB_DATABASE;
 
 if (!secretSession) {
   throw new Error('SESSION_CODE is not defined in environment variables');
 }
 
 const port = process.env.PORT || 3000;
-
 const app = express();
+const expireTime = 1000 * 60 * 60; // 1 hour
 
-const expireTime = 1000 * 60 * 60;
+let db;
+(async () => {
+  db = await connectToDatabase();
+})();
 
-var {database} = include('databaseConnection');
-
-const userCollection = database.db(data).collection('users');
-
-const mongoDB = MongoStore.create({
+const mongoStore = MongoStore.create({
   mongoUrl: `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/sessions`,
   crypto: {
     secret: dbSecret
@@ -34,9 +35,12 @@ const mongoDB = MongoStore.create({
   collectionName: 'sessions'
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
 app.use(session({
   secret: secretSession,
-  store: mongoDB,
+  store: mongoStore,
   saveUninitialized: false,
   resave: true,
   cookie: {
@@ -45,18 +49,27 @@ app.use(session({
   }
 }));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
-
-app.get('/', (req,res) => {
-  res.send("<h1>World Hello!</h1>");
+app.get('/', (req, res) => {
+  if (req.session.user) {
+    res.send(`
+      <h1>Hello, ${req.session.user.name}!</h1>
+      <a href="/members">Members Area</a><br>
+      <a href="/logout">Logout</a>
+    `);
+  } else {
+    res.send(`
+      <h1>Welcome</h1>
+      <a href="/signup">Sign up</a><br>
+      <a href="/login">Log in</a>
+    `);
+  }
 });
 
-app.get("*", (req,res) => {
-	res.status(404);
-	res.send("Page not found - 404");
-})
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found - 404");
+});
 
 app.listen(port, () => {
-	console.log("Node application listening on port "+port);
-}); 
+  console.log(`Server running on port ${port}`);
+});
